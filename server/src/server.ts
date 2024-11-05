2d42d9dfbdded4c9966191cac660b6ebc146d332
Ben Wright <bwright@2u.com>
2024-11-05 10:45:11 -0600

    removes the comment

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,6 @@ import express from "express";
 import db from "./config/connection.js";
 import routes from "./routes/index.js";
 
-// connect db
 await db();
 
 const PORT = process.env.PORT || 3001;
