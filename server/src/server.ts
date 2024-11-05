fdb04f3a91ae6f2ccdb21dc628e2748b2861fa48
Ben Wright <bwright@2u.com>
2024-11-05 10:44:18 -0600

    Adds a comment in the server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express from "express";
 
-import db from './config/connection.js';
-import routes from './routes/index.js';
+import db from "./config/connection.js";
+import routes from "./routes/index.js";
 
+// connect db
 await db();
 
 const PORT = process.env.PORT || 3001;
@@ -12,11 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/dist'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/dist"));
 
-   app.get('*', (_req, res) => {
-    res.sendFile('../client/dist/index.html');
+  app.get("*", (_req, res) => {
+    res.sendFile("../client/dist/index.html");
   });
 }
 
