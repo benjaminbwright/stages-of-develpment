ab226fc62c7b73a47bfa914dfc25a2d538c0ff06
Ben Wright <bwright@2u.com>
2024-11-06 01:21:15 -0600

    addes rollup optional dependencies

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,4 +10,11 @@ export default defineConfig({
     },
     specPattern: "cypress/component/**/*.cy.{js,ts,jsx,tsx}",
   },
+
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 });
